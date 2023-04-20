6e2b2d205e22b1c00095fe75bd6b8f82f63a9281
Gamaunov <gamaunov1911@gmail.com>
2023-04-20 15:33:10 +0300

    controlled input variant №1

---
+++ src/components/search/Search.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import s from './Search.module.scss';
+
+const Search = ({ searchValue, setSearchValue }) => {
+  return (
+    <div className={s.root}>
+      <svg
+        className={s.icon}
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        viewBox="0 0 16 16"
+      >
+        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+      </svg>
+      <input
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+        className={s.input}
+        placeholder="Поиск пицы..."
+      />
+      {searchValue && (
+        <svg
+          onClick={() => setSearchValue('')}
+          className={s.clearIcon}
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          viewBox="0 0 16 16"
+        >
+          <path d="M11.46.146A.5.5 0 0 0 11.107 0H4.893a.5.5 0 0 0-.353.146L.146 4.54A.5.5 0 0 0 0 4.893v6.214a.5.5 0 0 0 .146.353l4.394 4.394a.5.5 0 0 0 .353.146h6.214a.5.5 0 0 0 .353-.146l4.394-4.394a.5.5 0 0 0 .146-.353V4.893a.5.5 0 0 0-.146-.353L11.46.146zm-6.106 4.5L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z" />
+        </svg>
+      )}
+    </div>
+  );
+};
+
+export default Search;
