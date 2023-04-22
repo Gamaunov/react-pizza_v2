00275e932c543861af061cca1d4e6522fbd66e20
Gamaunov <gamaunov1911@gmail.com>
2023-04-22 11:36:20 +0300

    added focus on input

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -3,7 +3,15 @@ import { SearchContext } from '../../App';
 import s from './Search.module.scss';
 
 const Search = () => {
+  const inputRef = React.useRef();
+
+  const onClear = () => {
+    setSearchValue('');
+    inputRef.current.focus();
+  };
+
   const { searchValue, setSearchValue } = useContext(SearchContext);
+
   return (
     <div className={s.root}>
       <svg
@@ -17,6 +25,7 @@ const Search = () => {
         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
       </svg>
       <input
+        ref={inputRef}
         value={searchValue}
         onChange={(e) => setSearchValue(e.target.value)}
         className={s.input}
@@ -24,7 +33,7 @@ const Search = () => {
       />
       {searchValue && (
         <svg
-          onClick={() => setSearchValue('')}
+          onClick={onClear}
           className={s.clearIcon}
           xmlns="http://www.w3.org/2000/svg"
           width="16"
