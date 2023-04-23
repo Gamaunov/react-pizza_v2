ae8d40a3419ab3f2edde530b74c32f761b0827dc
Gamaunov <gamaunov1911@gmail.com>
2023-04-23 11:55:16 +0300

    created CartItem

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import { setSort } from '../redux/slices/filterSlice';
@@ -13,6 +13,7 @@ export const list = [
 ];
 
 const Sort = () => {
+  const sortRef = useRef();
   const dispatch = useDispatch();
   const sort = useSelector((state) => state.filter.sort);
 
@@ -23,8 +24,20 @@ const Sort = () => {
     setOpen(false);
   };
 
+  // useEffect(() => {
+  //   const handleClickOutside = (event) => {
+  //     if (event.path.includes(sortRef.current)) {
+  //       setOpen(false);
+  //     }
+  //   };
+
+  //   document.body.addEventListener('click', handleClickOutside);
+
+  //   return () => document.body.removeEventListener('click', handleClickOutside);
+  // }, []);
+
   return (
-    <div className="sort">
+    <div ref={sortRef} className="sort">
       <div className="sort__label">
         <svg
           width="10"
--- src/components/pizzaBlock/PizzaBlock.jsx
+++ src/components/pizzaBlock/PizzaBlock.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addItem } from '../../redux/slices/cartSlice';
 
 const PizzaBlock = ({
   id,
@@ -10,10 +12,28 @@ const PizzaBlock = ({
   category,
   rating,
 }) => {
+  const dispath = useDispatch();
+  const cartItem = useSelector((state) =>
+    state.cart.items.find((obj) => obj.id === id),
+  );
+  const addedCount = cartItem ? cartItem.count : 0;
+
   const [activeType, setActiveType] = useState(0);
   const [activeSize, setActiveSize] = useState(0);
 
   const typeNames = ['тонкое', 'традиционное'];
+
+  const onClickAdd = () => {
+    const item = {
+      id,
+      title,
+      price,
+      imageUrl,
+      type: typeNames[activeType],
+      size: sizes[activeSize],
+    };
+    dispath(addItem(item));
+  };
 
   return (
     <div className="pizza-block">
@@ -45,7 +65,10 @@ const PizzaBlock = ({
       </div>
       <div className="pizza-block__bottom">
         <div className="pizza-block__price">от {price} ₽</div>
-        <button className="button button--outline button--add">
+        <button
+          onClick={onClickAdd}
+          className="button button--outline button--add"
+        >
           <svg
             width="12"
             height="12"
@@ -59,7 +82,7 @@ const PizzaBlock = ({
             />
           </svg>
           <span>Добавить</span>
-          <i>0</i>
+          {addedCount > 0 && <i>{addedCount}</i>}
         </button>
       </div>
     </div>
